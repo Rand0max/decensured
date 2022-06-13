9f28db80dad7bc869ec8e49684054fad37c704b5
Rand0max <rand0max@protonmail.com>
2022-06-13 16:23:23 +0200

    Meta

---
+++ decensured.meta.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name        Décensured
+// @namespace   decensuredjvcom
+// @version     0.2.8
+// @downloadURL https://github.com/Rand0max/decensured/raw/master/decensured.user.js
+// @updateURL   https://github.com/Rand0max/decensured/raw/master/decensured.meta.js
+// @author      Rand0max
+// @description Contrer la censure et le 410 sur les forums JVC
+// @icon        https://risibank.fr/cache/medias/0/2/209/20968/thumb.png
+// @match       http://www.jeuxvideo.com/forums/*
+// @match       https://www.jeuxvideo.com/forums/*
+// @connect     myexternalip.com
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_addStyle
+// @grant       GM_deleteValue
+// @grant       GM_listValues
+// @grant       GM_getResourceText
+// @grant       GM.getValue
+// @grant       GM.setValue
+// @grant       GM.addStyle
+// @grant       GM.deleteValue
+// @grant       GM.listValues
+// @grant       GM.getResourceText
+// @grant       GM.xmlHttpRequest
+// @resource    DECENSURED_CSS https://raw.githubusercontent.com/Rand0max/decensured/master/decensured.css
+// @require     https://unpkg.com/gm-storage
+// @run-at      document-end
+// ==/UserScript==
+
