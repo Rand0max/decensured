3aa99e53bc7ed289178be8cf063e7db6dc17746d
Rand0max <rand0max@protonmail.com>
2022-06-16 23:28:30 +0200

    fix

--- decensured.meta.js
+++ decensured.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Décensured
 // @namespace   decensuredjvcom
-// @version     0.3.8
+// @version     0.3.9
 // @downloadURL https://github.com/Rand0max/decensured/raw/master/decensured.user.js
 // @updateURL   https://github.com/Rand0max/decensured/raw/master/decensured.meta.js
 // @author      Rand0max
